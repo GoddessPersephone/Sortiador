ecdb1323ce17ace5d2d184e62f25be0ad8dbc19c
Lara Veronica Leal <separatebusiness22@gmail.com>
2024-11-14 17:08:11 -0300

    Atualizando .gitignore para ignorar arquivos desnecessários

--- wwwroot/script.js
+++
@@ -1,4 +0,0 @@
-function gerarNumeroAleatorio() {
-    const numeroAleatorio = Math.floor(Math.random() * 100) + 1;
-    document.getElementById("resultado").textContent = `Numero gerado: ${numeroAleatorio}`;
-}
